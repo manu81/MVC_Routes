02fff904a8825afe982a37b5ac930df944f4a64c
Manuel <manuel.diaz@svcfin.com>
2018-12-28 11:47:29 -0500

    Initial

---
+++ MVC_Routes/MVC_Routes/Controllers/CuisineController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Routes.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        public ActionResult Search(string name)
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var nameParam = RouteData.Values["name"];
+
+            var result = String.Format("{0}:{1} {2}", controller, action, name);
+            return Content(result);
+        }
+    }
+}
